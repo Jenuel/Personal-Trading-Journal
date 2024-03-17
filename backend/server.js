fa342e26cff2c97b4a2f77aa685a731ade9666cd
Renuel Jeremi Balogo <renbalogo@gmail.com>
2024-03-17 15:40:40 +0800

    Added .env for environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
+require('dotenv').config()
 const express = require('express');
 
 const app = express()
 
-app.listen(4000, () => {
-    console.log("Listening on port 4000")
+app.get('/', (req, res) => {
+    res.json({mssg: "Hello World"})
+})
+ 
+app.listen(process.env.PORT, () => {
+    console.log("Listening on port", process.env.PORT)
 })
