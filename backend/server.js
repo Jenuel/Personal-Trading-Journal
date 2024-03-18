33be5f8c0ccdb83d8758b4337f0219febd6f82c4
Renuel Jeremi Balogo <renbalogo@gmail.com>
2024-03-18 22:15:27 +0800

    Second Commit for the changes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 require('dotenv').config()
 const express = require('express');
 const tradeRoutes = require('./routes/tradeRoute')
+const moongose = require('moongose')
 
 const app = express()
 
-
 app.use('/trades', tradeRoutes)
+
+mongoose.connect(process.env.DB_URI)
+ .then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log("Listening on port", process.env.PORT)
+    })
+ })
+ .catch((error) => {
+    console.log(error)
+ })
+
+
  
-app.listen(process.env.PORT, () => {
-    console.log("Listening on port", process.env.PORT)
-})
