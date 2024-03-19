0b815eb204c9a7266079539882a06533c8590e9d
Renuel Jeremi Balogo <renbalogo@gmail.com>
2024-03-19 11:02:50 +0800

    Added some comments for clearer view of the code

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,15 @@ const express = require('express');
 const tradeRoutes = require('./routes/tradeRoute')
 const moongose = require('moongose')
 
+//application
 const app = express()
 
+
+//routers
 app.use('/trades', tradeRoutes)
 
+
+//connection to database
 mongoose.connect(process.env.DB_URI)
  .then(() => {
     app.listen(process.env.PORT, () => {
