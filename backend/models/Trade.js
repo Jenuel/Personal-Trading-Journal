eeda01be9760229e5e455818e493bebca5da09b1
Renuel Jeremi Balogo <renbalogo@gmail.com>
2024-03-20 13:45:40 +0800

    Created the attributes for the trade object

--- backend/models/Trade.js
+++ backend/models/Trade.js
@@ -3,5 +3,44 @@ const moongose = require('moongose')
 const Schema = moongose.Schema
 
 const tradeSchema = new Schema({
-    
+    currencyPair: {
+        type: String, 
+        required: true
+    },
+    entryPrice: {
+        type: Number,
+        required: true
+    },
+    closingPrice: {
+        type: Number,
+        required: true
+    },
+    entryTime: {
+        type: Date,
+        required: true
+    },
+    closingTime: {
+        type: Date,
+        required: true
+    },
+    units: {
+        type: Number,
+        required: true
+    },
+    return: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    OvrBalance: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
 })
