bcfd256d36d2a63847f8cd4e5e130f4a4148b8bc
Renuel Jeremi Balogo <renbalogo@gmail.com>
2024-03-21 22:12:16 +0800

    Created the implementation for the createTrade function

--- backend/controllers/tradeController.js
+++ backend/controllers/tradeController.js
@@ -1,2 +1,29 @@
-const trade = require('./models/Trade.js')
+const Trade = require('./models/Trade.js')
 
+
+const createTrade = async (req, res) => {
+    const {
+        currencyPair,
+        entryPrice,
+        closingPrice,
+        entryTime,
+        closingTime,
+        units,
+        description
+    } = req.body
+
+    try {
+        const trade = await Trade.create({
+            currencyPair,
+            entryPrice,
+            closingPrice,
+            entryTime,
+            closingTime,
+            units,
+            description
+        })
+        res.status(200).json(trade)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
