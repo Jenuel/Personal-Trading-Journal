9c330751d87724f40b648dae28a78e3bcea80f3f
Renuel Balogo <renueljeremibalogo@gmail.com>
2024-06-02 15:07:11 +0800

    Initialized the navbar component

---
+++ frontend/client/src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='sidebar'>
+        <ul>
+            <li>
+                <a href="">Statistics</a>
+            </li>
+            <li>
+                <a href="">Trades</a>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
