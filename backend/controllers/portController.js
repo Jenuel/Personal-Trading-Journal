e0edd6d5f0f2a2f3036706c259de282e7b204da1
Renuel Balogo <renueljeremibalogo@gmail.com>
2024-06-04 16:55:38 +0800

    Implemented the functions for the CRUD operation of Portfolios

---
+++ backend/controllers/portController.js
@@ -0,0 +1,43 @@
+import { request } from 'express'
+import Portfolio from './models/Portfolio.js'
+
+const getPortfolios = async (request, response) => {
+    const portfolios = await Portfolio.find({}).sort({createdAt: -1})
+    response.status(200).send(portfolios)
+}
+
+const getPortfolio = async (request, response) => {
+    const { id } = request.params
+    const portfolio = await Portfolio.findById(id)
+    if (!portfolio){
+        res.status(404).json({error: 'No such trade'})
+    }
+    response.status(200).send(portfolio)
+}
+
+const createPortfolio = async (request, response) => {
+    const { body } = request
+    const newPortfolio = new Portfolio(body)
+    try {
+        const savedPortfolio = await newPortfolio.save();
+        return response.status(201).send(savedPortfolio)
+    } catch (error) {
+        return response.sendStatus(400)
+    }
+}
+
+const deletePortfolio = async (request, response) => {
+    const { id } = request.params
+    try {
+        const result = await Portfolio.findOneAndDelete({ _id: id });
+        if (result) {
+            return response.sendStatus(204);  
+        } else {
+            return response.sendStatus(404);  
+        }
+    } catch (error) {
+        return response.sendStatus(400);
+    }
+}
+
+export { getPortfolios, getPortfolio, createPortfolio, deletePortfolio }
