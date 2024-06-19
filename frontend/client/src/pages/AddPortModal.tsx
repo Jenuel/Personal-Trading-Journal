78de1336a0387549de0e12973dd44cde205b4922
Renuel Balogo <renueljeremibalogo@gmail.com>
2024-06-19 21:34:18 +0800

    Made the form for adding portfolios

---
+++ frontend/client/src/pages/AddPortModal.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+interface AddPortModalProps {
+    closeModal: () => void;
+}
+
+function AddPortModal({ closeModal } : AddPortModalProps) {
+    const [name, setName] = useState('')
+    const [balance, setBalance] = useState(0)
+
+    const handleSubmit = () => {
+
+    }
+
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+            <label>Name</label>
+            <input 
+            type="text"
+            required
+            value={name}
+            />
+            <label>Balance</label>
+            <input 
+            type="number"
+            required
+            value={balance}
+            />
+        </form>
+        <button className="submit" type='submit'>Create</button>
+        <button className="cancel" onClick={closeModal}>Cancel</button>
+    </div>
+  )
+}
+
+export default AddPortModal
