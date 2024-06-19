aa7153fcbeae907aaf54d4df6b69fa6fccb9097a
Renuel Balogo <renueljeremibalogo@gmail.com>
2024-06-19 19:31:26 +0800

    Added the add port button

--- frontend/client/src/components/AddPort.tsx
+++ frontend/client/src/components/AddPort.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { IconButton } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 
-function AddPortPopup() {
+interface AddPortPopupProps {
+  onClick?: () => void; // Optional prop for click handler
+}
+
+const AddPortPopup: React.FC<AddPortPopupProps> = ({ onClick }) => {
   return (
-    <IconButton color="primary" aria-label="add port">
+    <IconButton onClick={onClick} color="primary" aria-label="add port">
       <AddIcon />
     </IconButton>
   );
-}
+};
 
 export default AddPortPopup;
