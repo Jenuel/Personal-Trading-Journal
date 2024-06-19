0a5f1e0ccd4b63ff47fae78fd1d156c4a7f917b1
Renuel Balogo <renueljeremibalogo@gmail.com>
2024-06-19 21:34:58 +0800

    Handled the modal open and close function

--- frontend/client/src/components/AddPort.tsx
+++ frontend/client/src/components/AddPort.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
-import { IconButton } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
+import React, { useState } from 'react'
+import { IconButton } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
+import AddPortModal from '../pages/AddPortModal'
 
 interface AddPortPopupProps {
   onClick?: () => void; // Optional prop for click handler
 }
 
-const AddPortPopup: React.FC<AddPortPopupProps> = ({ onClick }) => {
+const AddPortPopup = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(true);
+  }
+
+  const handleClose = () => {
+    setIsOpen(false);
+  }
   return (
-    <IconButton onClick={onClick} color="primary" aria-label="add port">
-      <AddIcon />
-    </IconButton>
-  );
-};
+    <>
+      <IconButton onClick={handleOpen} color="primary" aria-label="add port">
+        <AddIcon />
+      </IconButton>
+
+      {isOpen && <AddPortModal closeModal={handleClose} />}
+    </>
+  )
+}
 
 export default AddPortPopup;
