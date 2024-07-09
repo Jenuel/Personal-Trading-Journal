5af9c87335e57a9aa5dc77cfa830159d62913fa2
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-07-09 20:10:34 +0800

    Fixed the issue in assynchronous rendering by adding a loading screen

--- frontend/client/src/pages/Layout.tsx
+++ frontend/client/src/pages/Layout.tsx
@@ -1,42 +1,46 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Navbar from '../components/Navbar'
-
-interface Trade {
+import './Layout.css'
+interface Portfolio {
     id: string
     name: string
 }
 
 interface LayoutProps {
-    data: Trade[]
+    data: Portfolio[]
     page: React.ReactNode
 }
 
-function Layout({data, page}: LayoutProps) {
+function Layout({ data, page }: LayoutProps) {
     const params = useParams()
     const portId = params.portId
 
-    const [portName, setPortName] = useState('');
+    const [portfolio, setPortfolio] = useState<Portfolio | undefined>();
 
     useEffect(() => {
+        console.log("Testing")
         const portfolio = data.find(data => data.id === portId )
         if (portfolio) {
-            setPortName(portfolio.name)
+            setPortfolio(portfolio)
         }
     }, [portId, data])
 
 
+    if (!portfolio) {
+        return <div>Loading...</div>; 
+    }
 
-  return (
-    <div className='main-container'>
-        <div className="sidebar">
-            <Navbar chosenPort={portName} portfolios={data}/>
+    return (
+        <div className='main-container'>
+            <div className="sidebar-container">
+                <Navbar chosenPort={portfolio} portfolios={data}/>
+            </div>
+            <div className="page">
+                {page}
+            </div>
         </div>
-        <div className="page">
-            {page}
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Layout
