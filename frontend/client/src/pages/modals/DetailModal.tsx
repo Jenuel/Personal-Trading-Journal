3f22797d0e51ba4ef1ad9a9ad9b572a54fc62da4
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-08-10 15:56:43 +0800

    Initialized the implementation of DetailModal

--- frontend/client/src/pages/modals/DetailModal.tsx
+++ frontend/client/src/pages/modals/DetailModal.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Trade } from '../../interfaces/interfaces'
+import './DetailModal.css'
 
 interface DetailModalProps {
   open: boolean
@@ -8,8 +9,25 @@ interface DetailModalProps {
 }
 
 function DetailModal({ open, onClose, data } : DetailModalProps) {
+  if (!open) return null
+
   return (
-    <div>DetailModal</div>
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button className="modal-close-button" onClick={onClose}>X</button>
+        <div>
+          <p>Type: {data.category}</p>
+          <p>Currency Pair: {data.currencyPair}</p>
+          <p>Entry Price: {data.entryPrice}</p>
+          <p>Closing Price: {data.closingPrice}</p>
+          <p>Entry Time: {data.entryTime.toString()}</p>
+          <p>Closing Time: {data.closingTime.toString()}</p>
+          <p>Units: {data.units}</p>
+          <p>Return: {data.return}</p>
+          <p>Description: {data.description}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
