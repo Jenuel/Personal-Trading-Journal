96f6dfc1f1045bbb71512fd4870c3043bc26a872
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-08-29 16:09:41 +0800

    Cleaned the statistics page for restart

--- frontend/client/src/pages/statistics/Statistics.tsx
+++ frontend/client/src/pages/statistics/Statistics.tsx
@@ -1,61 +1,17 @@
 import React from 'react'
 import '../statistics/Statistics.css'
-import AddTrade from '../../components/AddTrade'
+import {Chart as ChartJS } from "chart.js/auto"
+import { Bar, Doughnut, Line } from "react-chartjs-2"
+
 
 function Statistics() {
   return (
-    <div className='statistics-container'>
-      <div className="table-container">
-        <div className="contents">
-          <div className="filter">
-            <AddTrade />
-          </div>
-          <div className="header-container">
-            <tr>
-              <th>Testing</th>
-              <th>Testing</th>
-              <th>Testing</th>
-              <th>Testing</th>
-              <th>Testing</th>
-            </tr>
-          </div>
-          <div className="trades-container">
-          <tr>
-            <td>Testing</td>
-            <td>Testing</td>
-            <td>Testing</td>
-            <td>Testing</td>
-            <td>Testing</td>
-            <td>Testing</td>
-          </tr>
-          <tr>
-            <td>Testing</td>
-            <td>Testing</td>
-            <td>Testing</td>
-            <td>Testing</td>
-            <td>Testing</td>
-            <td>Testing</td>
-          </tr>
-          <tr>
-            <td>Testing</td>
-            <td>Testing</td>
-            <td>Testing</td>
-            <td>Testing</td>
-            <td>Testing</td>
-            <td>Testing</td>
-          </tr>
-          <tr>
-            <td>Testing</td>
-            <td>Testing</td>
-            <td>Testing</td>
-            <td>Testing</td>
-            <td>Testing</td>
-            <td>Testing</td>
-          </tr>
-          </div>
-        </div>
+   <div className="statistics">
+      <div className="bar-container">
+
+
       </div>
-    </div>
+   </div>
   )
 }
 
