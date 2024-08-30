2e08be442617fb21b0b8aa0100c12317e0f2c0de
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-08-30 23:10:54 +0800

    Integrated the controllers to the port routes

--- backend/routes/portRoute.js
+++ backend/routes/portRoute.js
@@ -1,21 +1,14 @@
 import express from 'express'
-
+import { getPortfolios, getPortfolio, createPortfolio, deletePortfolio } from '../controllers/portController'
 const router = express.Router()
 
+router.get('/ports', getPortfolios)
 
-router.get()
+router.get('/ports/:id', getPortfolio)
 
+router.post('/ports/:id', createPortfolio)
 
-router.get()
-
-
-router.post()
-
-
-router.put()
-
-
-router.delete()
+router.delete('/ports', deletePortfolio)
 
 
 export default router;
