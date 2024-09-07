ad662f01fb07c415f24d7aa8b947883d95b1f56b
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-09-07 22:55:02 +0800

    Added the useEffect hook to fetch portData

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import LandingPage from './pages/landing/LandingPage';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
@@ -6,35 +6,24 @@ import Layout from './pages/Layout';
 import Statistics from './pages/statistics/Statistics';
 import Transactions from './pages/transactions/Transactions';
 import { Portfolio } from '../src/interfaces/interfaces'
-
+import axios from 'axios';
 
 function App() {
 
-  const samplePorts: Portfolio[] = [
-    { id: '1', name: 'Trade A' },
-    { id: '2', name: 'Trade B' },
-    { id: '3', name: 'Trade C' },
-    { id: '4', name: 'Trade D' },
-    { id: '5', name: 'Trade E' },
-    { id: '6', name: 'Trade F' },
-    { id: '7', name: 'Trade G' },
-    { id: '8', name: 'Trade H' },
-    { id: '9', name: 'Trade I' },
-    { id: '10', name: 'Trade J' },
-    { id: '11', name: 'Trade K' },
-    { id: '12', name: 'Trade L' },
-    { id: '13', name: 'Trade M' },
-    { id: '14', name: 'Trade N' },
-    { id: '15', name: 'Trade O' },
-    { id: '16', name: 'Trade P' },
-    { id: '17', name: 'Trade Q' },
-    { id: '18', name: 'Trade R' },
-    { id: '19', name: 'Trade S' },
-    { id: '20', name: 'Trade T' }
-];
+  const [data, setData] = useState<Portfolio[]>([])
+  const [loading, setLoading] = useState(true)
 
+  useEffect(() => {
+    axios.get('http://localhost:4000/ports')
+    .then(res =>{
+      setData(res.data)
+      setLoading(false)
+    }).catch(err => {
+      console.error(err);
+      setLoading(false)
+    })
+  }, [])
 
-const [data, setData] = useState(samplePorts)
 
 const router = createBrowserRouter([
   { //root page aka Landing page
