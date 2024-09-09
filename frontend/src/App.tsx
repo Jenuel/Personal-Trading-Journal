ca51eed2c5f088a469a742a8a83f8e4ff983dda0
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-09-09 14:58:40 +0800

    Modified the GET api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,45 +5,50 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Statistics from './pages/statistics/Statistics';
 import Transactions from './pages/transactions/Transactions';
-import { Portfolio } from '../src/interfaces/interfaces'
+import { Portfolio } from '../src/interfaces/interfaces';
 import axios from 'axios';
 
 function App() {
-
-  const [data, setData] = useState<Portfolio[]>([])
-  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState<Portfolio[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get('http://localhost:4000/ports')
-    .then(res =>{
-      setData(res.data)
-      setLoading(false)
-    }).catch(err => {
-      console.error(err);
-      setLoading(false)
-    })
-  }, [])
+    const fetchPortfolios = async () => {
+      try {
+        const response = await axios.get('http://localhost:4000/ports');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching portfolios:', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+  
+    fetchPortfolios();
+  }, []);
 
+  const router = createBrowserRouter([
+    { // Root page aka Landing page
+      path: "/",
+      element: <LandingPage data={data} />
+    },
+    { // Statistics page
+      path: "/statistics/:portId",
+      element: <Layout data={data} page={<Statistics />} />
+    },
+    { // Transactions page
+      path: "/transactions/:portId",
+      element: <Layout data={data} page={<Transactions />} />
+    }
+  ]);
 
-const router = createBrowserRouter([
-  { //root page aka Landing page
-    path: "/",
-    element: <LandingPage data={data}/>
-  },
-  { //Statistics page
-    path: "/statistics/:portId",
-    element: <Layout data={data} page={<Statistics/>} />
-  },
-  { //Trades page
-    path: "/transactions/:portId",
-    element: <Layout data={data} page={<Transactions/>} />
+  if (loading) {
+    return <div>Loading...</div>;
   }
-])
 
   return (
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   );
-
 }
 
 export default App;
