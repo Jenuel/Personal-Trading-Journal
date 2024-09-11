74638401cee3ccb1e5b42ba535d4f1d27beb3b73
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-09-11 11:30:20 +0800

    Omiited the update and version number from the timestamp

--- backend/models/Portfolio.js
+++ backend/models/Portfolio.js
@@ -1,18 +1,26 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-    const newPortfolioSchema = new Schema({
-        portName: {
-            type: String,
-            required: true
-        },
-        balance: {
-            type: Number,
-            required: true
-        }
-    }, { timestamps: true})
+const newPortfolioSchema = new Schema({
+    portName: {
+        type: String,
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true });
 
-const Portfolio = mongoose.model('portfolios', newPortfolioSchema)
+newPortfolioSchema.set('toJSON', {
+    transform: function (doc, ret, options) {
+        delete ret.__v;
+        delete ret.updatedAt;
+        return ret;
+    }
+});
+
+const Portfolio = mongoose.model('Portfolio', newPortfolioSchema);
 
 export default Portfolio;
