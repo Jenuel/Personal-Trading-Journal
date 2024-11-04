25ea4f5dce0702c9830bd8e52b5211091407b741
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-11-04 18:59:50 +0800

    Wrapped the application for the provider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { TradesContextProvider } from './context/TradeContext';
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App/>
+  <TradesContextProvider>
+    <App/>
+  </TradesContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
