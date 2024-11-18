527c9cc6288ac70c47df09b340dd0eb39ec47879
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-11-18 16:27:26 +0800

    Allowed patch in CORS

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(cors({
     origin: 'http://localhost:3000', 
-    methods: 'GET,POST,PUT,DELETE', 
+    methods: 'GET,PATCH,POST,PUT,DELETE', 
   }));
 //routers
 app.use(express.json()); 
