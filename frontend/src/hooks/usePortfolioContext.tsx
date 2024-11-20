0c38e9115a660efc6a01d755ad450aabdb4fc942
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-11-20 21:20:03 +0800

    implemented the custom hook for the ports

---
+++ frontend/src/hooks/usePortfolioContext.tsx
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { PortfolioContext } from "../context/PortContext";
+
+export const usePortfolioContext = () => {
+  const context = useContext(PortfolioContext);
+
+  if (!context) {
+    throw new Error("usePortfolioContext must be used within a PortfolioContextProvider");
+  }
+  return context;
+};
