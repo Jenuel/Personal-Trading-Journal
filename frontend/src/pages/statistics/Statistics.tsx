6725e72ba9a673543c03a43a60a0708ef054031b
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-12-05 13:45:22 +0800

    Integrated the charts

--- frontend/src/pages/statistics/Statistics.tsx
+++ frontend/src/pages/statistics/Statistics.tsx
@@ -1,18 +1,71 @@
-import React from 'react'
-import '../statistics/Statistics.css'
-import {Chart as ChartJS } from "chart.js/auto"
-import { Bar, Doughnut, Line } from "react-chartjs-2"
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../statistics/Statistics.css";
+import LineGraph from "./graphs/LineGraph";
+import { useParams } from "react-router-dom";
+import DoughnotChart from "./graphs/DoughnutGraph";
+import BarChart from "./graphs/BarChart";
+import AccumulatedReturnsChart from "./graphs/AccumulatedReturnsChart";
 
 function Statistics() {
+  const { portId } = useParams<{ portId: string }>();
+  const [port, setPort] = useState<{ initialBalance: number } | undefined>();
+  const [trades, setTrades] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchTrades = async () => {
+      try {
+        setLoading(true);
+        const res = await axios.get(`http://localhost:4000/trades/port/${portId}`);
+        setTrades(res.data);
+        console.log(res.data);
+      } catch (err: any) {
+        console.error("Error fetching trades:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    const fetchPort = async () => {
+      try {
+        const res = await axios.get(`http://localhost:4000/ports/${portId}`);
+        setPort(res.data);
+      } catch (error: any) {
+        console.error("Error:", error);
+      }
+    };
+
+    if (portId) {
+      fetchTrades();
+      fetchPort();
+    }
+  }, [portId]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!port) {
+    return <p>Error loading portfolio data</p>;
+  }
+
   return (
-   <div className="statistics">
+    <div className="statistics">
       <div className="bar-container">
-
-
+        <LineGraph trades={trades} initialPortfolioValue={port.initialBalance} />
       </div>
-   </div>
-  )
+      <div className="bar-container">
+        <DoughnotChart trades={trades} />
+      </div>
+      <div className="bar-container">
+        <BarChart trades={trades} />
+      </div>
+      <div className="bar-container">
+        <AccumulatedReturnsChart trades={trades} />
+      </div>
+    </div>
+  );
 }
 
-export default Statistics
+export default Statistics;
