5df529a9b2135c60f37af515b1f30179457114e7
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-06-28 12:58:12 +0800

    Implement PostgreSQL database connection and schema setup

--- backend/server.js
+++ backend/server.js
@@ -4,42 +4,28 @@ import tradeRoutes from './routes/tradeRoute.js';
 import portRoutes from './routes/portRoute.js';
 import mongoose from 'mongoose';
 import cors from 'cors'
+import { connectDB } from './db.js';
 
 dotenv.config();
-//application
+
 const app = express()
 
 app.use(cors({
     origin: 'http://localhost:3000', 
     methods: 'GET,PATCH,POST,PUT,DELETE', 
   }));
+
 //routers
 app.use(express.json()); 
 app.use(portRoutes)
 app.use(tradeRoutes)
 
-
-//connection to database
-mongoose.connect(process.env.DB_URI)
- .then(() => {
+try {
+    await connectDB();
     app.listen(process.env.PORT, () => {
         console.log("Listening on port", process.env.PORT || 3000)
     })
- })
- .catch((error) => {
-    console.log(error)
- })
-
- /* ANOTHER SOLUTION OF SPECIFYING THE DATABASE INSTEAD OF 
-    REFACTORING THE MONGO CLUSTER URI
- try {
-    await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        dbName: 'ecomm' // specify the database name here
-    });
-    console.log('Connected to MongoDB');
 } catch (error) {
-    console.error('Database connection error:', error);
+    console.error("Error in server.js", error)
+    process.exit(1);
 }
- */
