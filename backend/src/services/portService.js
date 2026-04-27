5244c5b388d3909e53668ab7224f3b28366fafd6
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2026-04-27 17:58:12 +0800

    feat: add Portfolio and Trade services with CRUD operations

---
+++ backend/src/services/portService.js
@@ -0,0 +1,53 @@
+import { portfolioRepository } from "../repositories/portRepo.js";
+
+export const PortfolioService = {
+    createPortfolio: async (name, balance) => {
+        const results = await portfolioRepository.createPortfolio(name, balance);
+
+        if (!results) {
+            throw new Error('Failed to create portfolio');
+        }
+
+        return results;
+    },
+
+    getAllPortfolios: async () => {
+        const results = await portfolioRepository.getAllPortfolios();
+
+        if (!results) {
+            throw new Error('Failed to fetch portfolios');
+        }
+
+        return results;
+    },
+
+    getPortfolio: async (id) => {
+        const results = await portfolioRepository.getPortfolio(id);
+
+        if (!results) {
+            throw new Error('Failed to fetch portfolio');
+        }
+
+        return results;
+    },
+
+    updateBalance: async (id, balance) => {
+        const results = await portfolioRepository.updateBalance(id, balance);
+
+        if (!results) {
+            throw new Error('Failed to update balance');
+        }
+
+        return results;
+    },
+
+    deletePortfolio: async (id) => {
+        const results = await portfolioRepository.deletePortfolio(id);
+
+        if (!results) {
+            throw new Error('Failed to delete portfolio');
+        }
+
+        return results;
+    }
+}
---
+++ backend/src/services/tradeService.js
@@ -0,0 +1,53 @@
+import { tradeRepository } from "../repositories/tradeRepo.js";
+
+export const TradeService = {
+    createTrade: async (trade) => {
+        const results = await tradeRepository.createTrade(trade);
+
+        if (!results) {
+            throw new Error('Failed to create trade');
+        }
+
+        return results;
+    },
+
+    getAllTrades: async () => {
+        const results = await tradeRepository.getAllTrades();
+
+        if (!results) {
+            throw new Error('Failed to fetch trades');
+        }
+
+        return results;
+    },
+
+    getTrade: async (id) => {
+        const results = await tradeRepository.getTrade(id);
+
+        if (!results) {
+            throw new Error('Failed to fetch trade');
+        }
+
+        return results;
+    },
+
+    updateTrade: async (id, trade) => {
+        const results = await tradeRepository.updateTrade(id, trade);
+
+        if (!results) {
+            throw new Error('Failed to update trade');
+        }
+
+        return results;
+    },
+
+    deleteTrade: async (id) => {
+        const results = await tradeRepository.deleteTrade(id);
+
+        if (!results) {
+            throw new Error('Failed to delete trade');
+        }
+
+        return results;
+    }
+}
